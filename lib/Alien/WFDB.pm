6acc0ca24536f0288314e1b17214888752600e05
Zakariyya Mughal <zmughal@cpan.org>
2013-12-28 21:38:15 +0000

    Import of ZMUGHAL/Alien-WFDB-0.001 from CPAN.
    gitpan-cpan-distribution: Alien-WFDB
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         ZMUGHAL/Alien-WFDB-0.001.tar.gz
    gitpan-cpan-author:       ZMUGHAL
    gitpan-cpan-maturity:     released

---
+++ lib/Alien/WFDB.pm
@@ -0,0 +1,43 @@
+package Alien::WFDB;
+{
+  $Alien::WFDB::VERSION = '0.001';
+}
+
+use strict;
+use warnings;
+
+use parent 'Alien::Base';
+
+1;
+# ABSTRACT: Alien package for the WFDB (WaveForm DataBase) library
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Alien::WFDB - Alien package for the WFDB (WaveForm DataBase) library
+
+=head1 VERSION
+
+version 0.001
+
+=head1 SEE ALSO
+
+L<WFDB|http://physionet.org/physiotools/wfdb.shtml>, L<Bio::Physio::WFDB>
+
+=head1 AUTHOR
+
+Zakariyya Mughal <zmughal@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Zakariyya Mughal.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
